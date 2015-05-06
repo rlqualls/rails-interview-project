0537106aa625d01f2328bc8f3921a90296cc1bf0
Brandon Richey <brichey@teladoc.com>
2015-05-06 10:58:32 -0400

    Initial commit

---
+++ db/migrate/20150506145755_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.integer :id
+      t.string :name
+      t.integer :group_id
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20150506145815_create_emails.rb
@@ -0,0 +1,11 @@
+class CreateEmails < ActiveRecord::Migration
+  def change
+    create_table :emails do |t|
+      t.integer :id
+      t.string :address
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
