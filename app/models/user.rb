3ce5dec24459a9dfaf9c0dd1d2aba6024e5a0ed8
Robert Qualls <robert@robertqualls.com>
2015-06-09 09:52:33 -0500

    Define models
    Defines Group, User, and Email models.
    TODO: Any needed association validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_one :email
+  accepts_nested_attributes_for :email, allow_destroy: true
+
+  validates :name, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "init" do
-    @user = User.create
-    assert true
+  self.use_transactional_fixtures = true
+
+  test "should be valid with a name" do
+    @user = User.create(name: "John")
+    assert @user.valid?
+  end
+
+  test "should not be valid without a name" do
+    @user = User.create(name: "")
+    assert @user.invalid?
   end
 end
